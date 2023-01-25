a2290a06b65c2eaa9dd988070741fc2fad7a66b1
WagaoCarvalhoDev <wagaocarvalhodev@gmail.com>
2023-01-25 11:09:05 -0300

    Initial commit

---
+++ lib/provider/provider_consumer_widget_page.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+void main() {
+  runApp(const ProviderScope(child: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        theme: ThemeData(primarySwatch: Colors.blue),
+        home: const MyHomePage(),
+      );
+}
+
+final myProvider = Provider((ref) => 'Test RiverPod');
+
+class MyHomePage extends ConsumerWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final name = ref.watch(myProvider);
+
+    return Scaffold(
+      body: Center(
+        child: Text(name),
+      ),
+    );
+  }
+}
