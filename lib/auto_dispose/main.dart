acb6ef78941b6a3b3c256b1a974aaf9edf3fba8f
WagaoCarvalhoDev <wagaocarvalhodev@gmail.com>
2023-01-25 22:56:42 -0300

    auto dispose init

---
+++ lib/auto_dispose/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+import 'counter.dart';
+import 'home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      routerConfig: _router,
+    );
+  }
+}
+
+final GoRouter _router = GoRouter(
+  routes: <RouteBase>[
+    GoRoute(
+      path: '/',
+      builder: (BuildContext context, GoRouterState state) {
+        return const Home();
+      },
+    ),
+    GoRoute(
+      path: '/counter',
+      builder: (BuildContext context, GoRouterState state) {
+        return const Counter();
+      },
+    ),
+  ],
+);
